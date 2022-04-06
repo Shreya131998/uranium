1862a6b8b1d9461dad222dc59bcc0da9c6e5b0ef
Shreya131998 <singh.shreya0741416@gmail.com>
2022-04-06 20:42:28 +0530

    my first git assignment

--- src/routes/logger.js
+++
@@ -1,5 +0,0 @@
-let endpoint="https://www.google.com"
-let log =function(){
-    console.log("Hi")
-}
-module.exports.endpoint=endpoint
---
+++ src/util/helper.js
@@ -0,0 +1,13 @@
+const printDate=function(){
+    console.log("04-04-2022")
+}
+const printMonth=function(){
+    console.log("04")
+}
+const getBatchInfo=function(){
+    console.log("Uranium,3rd Week,Wednesday,the topic being taught today is Nodejs module system")
+}
+
+module.exports.printDate=printDate
+module.exports.printMonth=printMonth
+module.exports.getBatchInfo=getBatchInfo
