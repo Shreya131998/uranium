1a1d23bf47e221f424b7c679ddba4cf88918493e
Shreya131998 <singh.shreya0741416@gmail.com>
2022-04-06 18:10:07 +0530

    my assignment

---
+++ src/routes/logger.js
@@ -0,0 +1,5 @@
+let endpoint="https://www.google.com"
+let log =function(){
+    console.log("Hi")
+}
+module.exports.endpoint=endpoint
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const newone=require("./logger");
 
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
     res.send('My first ever api!')
+    console.log(newone.endpoint)
+    
+    
+    
 });
 
 module.exports = router;
