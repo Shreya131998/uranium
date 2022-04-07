33665f0991ad3e17c8381cfc20b1c07c1e6fecf6
Shreya131998 <singh.shreya0741416@gmail.com>
2022-04-07 18:59:20 +0530

    my assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -11,6 +11,28 @@ router.get('/test-me', function (req, res) {
     res.send('My first ever api!')
 });
 
+router.get('/all-candidates', function (req, res) {
+    const arr=["jan","feb","mar","apr","may","june","july","aug","sept","oct"]
+    
+    res.send(arr)
+    
+    
+});
+
+router.get('/candidates', function (req, res) {
+    const value1=req.query.count
+    
+     let arr1=[]
+     let arr=["jan","feb","mar","apr","may","june","july","aug","sept","oct"]
+     for(let i=0;i<value1;i++){
+         
+         arr1.push(arr[i])
+     }
+     res.send(arr1)
+    
+    
+});
+
 
 
 
