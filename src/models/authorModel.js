aec119ed205866160dc19411fa2d93766ec58643
Shreya131998 <singh.shreya0741416@gmail.com>
2022-04-14 21:02:04 +0530

    populate-ref

---
+++ src/models/authorModel.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const authorSchema = new mongoose.Schema( {
+    author_id:Number,
+    author_name : String,
+    age :Number,
+    adress : String
+} )
+
+module.exports = mongoose.model('Author', authorSchema)
